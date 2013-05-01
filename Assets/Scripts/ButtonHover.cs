d0d11915e6674bab683d5289a07e861cb7ac42ae
unknown <knp4816@C2435-6.main.ad.rit.edu>
2013-05-01 16:46:06 -0400

    Made changes to the menu. Added credits and credit button.

--- Assets/Scripts/ButtonHover.cs
+++ Assets/Scripts/ButtonHover.cs
@@ -5,9 +5,13 @@ public class ButtonHover : MonoBehaviour {
 	//variables
 	public GameObject[] buttons;
 	public GameObject[] selectorIcon;
-	int index = 0;
+	public GameObject[] backArray;
+	int index=0;
+	int backIndex=0;
+	bool mainMenu = true;
 	int prevIndex=0;
 	bool btnReset = true;
+	bool backBtn = false;
 	
 	// Use this for initialization
 	void Start () {
@@ -15,6 +19,14 @@ public class ButtonHover : MonoBehaviour {
 		buttons[0].renderer.material.color= Color.cyan;
 		selectorIcon[0].renderer.enabled =true;
 		selectorIcon[1].renderer.enabled =false;
+		selectorIcon[2].renderer.enabled =false;
+		
+		backArray[0].renderer.material.color = Color.cyan;
+		//backArray[1].renderer.enabled = true;
+		
+		/*index = 2;
+		prevIndex = 2;*/
+		
 	}
 	
 	// Update is called once per frame
@@ -27,46 +39,65 @@ public class ButtonHover : MonoBehaviour {
 	// Shows which button is selected
 	void hoverOver()
 	{
-		if(btnReset)
+		if (mainMenu)
 		{
-			prevIndex=index;
-			//RaycastHit press;
-			//bool hit = Physics.Raycast(Camera.mainCamera.ScreenPointToRay(Input.mousePosition), out press);
-			
-			// handles the switching between buttons
-			if(Input.GetAxis("Vertical")<0)
+			if(btnReset)
 			{
-				index--;
-				if(index < 0)
+				prevIndex=index;
+				//RaycastHit press;
+				//bool hit = Physics.Raycast(Camera.mainCamera.ScreenPointToRay(Input.mousePosition), out press);
+				
+				// handles the switching between buttons
+				if(Input.GetAxis("Vertical")>0)
 				{
-					index= buttons.Length -1;
+					/*
+					index--;
+					if(index >= buttons.Length)
+					{
+						index = 0;
+					}
+					*/
+					index--;
+					if(index < 0)
+					{
+						index= buttons.Length -1;
+					}
+					
+					selectorIcon[index].renderer.enabled = true;
+					selectorIcon[prevIndex].renderer.enabled =false;
+					
+					buttons[index].renderer.material.color= Color.cyan;
+					buttons[prevIndex].renderer.material.color= Color.white;
+					
+					btnReset=false;
 				}
-				
-				selectorIcon[prevIndex].renderer.enabled =false;
-				selectorIcon[index].renderer.enabled = true;
-				
-				buttons[prevIndex].renderer.material.color= Color.white;
-				buttons[index].renderer.material.color= Color.cyan;
-				btnReset=false;
+				else if(Input.GetAxis ("Vertical")<0)
+				{	
+					//*
+					index++;
+					if(index >= buttons.Length)
+					{
+						index = 0;
+					}
+					/*/
+					index++;
+					if(index < 0)
+					{
+						index= buttons.Length -1;
+					}
+					//*/
+					selectorIcon[prevIndex].renderer.enabled =false;
+					selectorIcon[index].renderer.enabled = true;
+					
+					buttons[prevIndex].renderer.material.color= Color.white;
+					buttons[index].renderer.material.color= Color.cyan;
+					btnReset=false;
+				}
 			}
-			else if(Input.GetAxis ("Vertical")>0)
+			else if(Input.GetAxis("Vertical")==0)
 			{
-				index++;
-				if(index >= buttons.Length)
-				{
-					index = 0;
-				}
-				selectorIcon[prevIndex].renderer.enabled =false;
-				selectorIcon[index].renderer.enabled = true;
-				
-				buttons[prevIndex].renderer.material.color= Color.white;
-				buttons[index].renderer.material.color= Color.cyan;
-				btnReset=false;
+				btnReset=true;
 			}
-		}
-		else if(Input.GetAxis("Vertical")==0)
-		{
-			btnReset=true;
 		}
 	}
 	
@@ -74,9 +105,30 @@ public class ButtonHover : MonoBehaviour {
 	{
 		if(Input.GetButtonUp("select"))
 		{
-			MonoBehaviour btn = buttons[index].GetComponent("hover Script") as MonoBehaviour;
-			btn.SendMessage("ButtonSelected",SendMessageOptions.RequireReceiver);			
+			if (backBtn)
+			{
+				GameObject.Find("Main Camera").GetComponent<MoveCamera>().ChangeCamera(MoveCamera.CamPositions.main);
+				mainMenu = true;
+				backBtn = false;
+				
+			}
+			else if(index == 1)
+			{
+				GameObject.Find("Main Camera").GetComponent<MoveCamera>().ChangeCamera(MoveCamera.CamPositions.credits);
+				mainMenu = false;
+				backBtn = true;
+				
+			}
+			else
+			{
+				MonoBehaviour btn = buttons[index].GetComponent("hover Script") as MonoBehaviour;
+				btn.SendMessage("ButtonSelected",SendMessageOptions.RequireReceiver);
+			}
 			
+			/* if(backArray[0])
+			{
+				GameObject.Find("Main Camera").GetComponent<MoveCamera>().ChangeCamera(MoveCamera.CamPositions.main);	
+			}*/
 		}
 	}
 }

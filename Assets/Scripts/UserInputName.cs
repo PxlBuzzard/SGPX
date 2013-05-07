3f092f500454d8577ef47a459adfffee63d5abbc
unknown <knp4816@OH1380-14.main.ad.rit.edu>
2013-05-07 13:43:29 -0400

    Put in the new logo.

--- Assets/Scripts/UserInputName.cs
+++ Assets/Scripts/UserInputName.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class UserInputName : MonoBehaviour
 {
+	//constants
 	private const string SECRET_KEY = "mySecretKey";
 	private const string GET_USER_URL = "http://sgpx.coldencullen.com/php/getuser.php?";
 	string hash = SECRET_KEY;
 	
+	// variables
 	private string playerName = "";
 	public GUIStyle TextStyle = new GUIStyle();
 	public int fontSize; 
@@ -32,11 +34,14 @@ public class UserInputName : MonoBehaviour
 	{
 		
 	}
-	
+	// draw the gui for the player to input their name
 	void OnGUI()
 	{
+		// adjusts as the screen size changes
+		// helps the textfield stay in the same place (somewhat)
 		float halfScH = Screen.height/2;
 		float halfScW = Screen.width/2;
+		//textfield for player in input their name
 		playerName = GUI.TextField( new Rect((halfScW/2),( halfScH),(halfScW),(halfScH/4)), playerName, TextStyle );
 	}
 }
